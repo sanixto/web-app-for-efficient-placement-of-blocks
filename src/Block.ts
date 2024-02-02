fbe62282f5260ff2544cd4712ee62f3b51e94d60
sanixto <sanix.to@gmail.com>
2024-02-02 18:55:42 +0200

    update Block class

--- src/Block.ts
+++ src/Block.ts
@@ -1,10 +1,31 @@
 export default class Block {
   #width: number;
   #height: number;
+  initOrder: number;
+  coords: {
+    top: number,
+    left: number,
+    bottom: number,
+    right: number,
+  };
+  isSurrounded = false;
 
-  constructor(width: number, height: number) {
+  constructor(width: number, height: number, initOrder: number) {
     this.#width = width;
     this.#height = height;
+    this.initOrder = initOrder;
+  }
+
+  get width() {
+    return this.#width;
+  }
+
+  get height() {
+    return this.#height;
+  }
+
+  setCoords(left: number, right: number, top: number, bottom: number): void {
+    this.coords = { left, right, top, bottom };
   }
 
   getArea(): number {
@@ -13,5 +34,16 @@ export default class Block {
 
   rotateBy90(): void {
     [this.#height, this.#width] = [this.#width, this.#height];
+  }
+
+  draw(container: HTMLElement): void {
+    const blockElem = document.createElement('div');
+    blockElem.style.width = `${this.#width}px`;
+    blockElem.style.height = `${this.#height}px`;
+    blockElem.style.top = `${this.coords.top}px`;
+    blockElem.style.left = `${this.coords.left}px`;
+    blockElem.className = 'block';
+    blockElem.innerHTML = `<div>${this.initOrder}</div>`;
+    container.appendChild(blockElem);
   }
 }
